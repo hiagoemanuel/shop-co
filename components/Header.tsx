ce644afe5618ccae204c4ded27844aa04cc492d9
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-12-12 23:14:59 -0300

    initial setup

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import { Search } from './svgs/Search'
+
+export const Header = () => {
+  return (
+    <header>
+      <Search />
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'integral-cf': 'var(--font-integral-cf)',
+      satoshi: 'var(--satoshi)',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        cyan: 'var(--cyan)',
+        golden: 'var(--golden)',
+        green: 'var(--green)',
+        red: 'var(--red)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
